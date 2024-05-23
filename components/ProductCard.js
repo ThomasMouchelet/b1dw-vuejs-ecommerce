7dd9ca20ab5c8a2a9ef7c321895bc92813d64ce9
Thomas Mouchelet <mouchelet.thomas@gmail.com>
2024-05-23 10:15:01 +0200

    Add cart list items

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -4,17 +4,25 @@ export const ProductCard = {
         price: Number,
         src: String,
     },
+    methods: {
+        emithello() {
+            console.log('Hello from child');
+          this.$emit('hello');
+        }
+    },
     template: `
-        <div class="card" :key="id">
+        <div class="card" :key="id" >
             <img :src="src" alt="">
             <span class="price">{{price}} €</span>
             <div>
                 <button class="btn btn-secondary">
                     <i class="fa-regular fa-heart"></i>
                 </button>
-                <button class="btn btn-primary">
+                <button class="btn btn-primary" @click="$emit('handleaddtocard', id)">
                     <i class="fa-solid fa-cart-shopping"></i>
                 </button>
+                <button @click="emithello">Hello</button>
+                    
             </div>
         </div>
     `   
--- script.js
+++ script.js
@@ -8,7 +8,54 @@ createApp({
   },
   data(){
     return {
-      itemList: []
+      itemList: [],
+      cartItemList: [],
+      total: 0,
+    }
+  },
+  methods: {
+    parenthellofunction() {
+      console.log('Hello from parent');
+    },
+    handleaddtocard(id) {
+      console.log("click to add button element", id)
+      // increase number to item into itemList
+      const item = this.itemList.find(item => item.id === id)
+      item.number++
+      console.log("this.itemList : ", this.itemList)
+      // check all item into itemList 
+        // if number > 0, add item into cartItemList
+      this.cartItemList = this.itemList.filter(item => item.number > 0)
+      this.updatetotalprice()
+    },
+    decreasenumber(id) {
+      console.log("click to decrease button element", id)
+      // find element with id
+      const item = this.itemList.find(item => item.id === id)
+      item.number--
+      // check if number < or equal to 0: delete
+      if(item.number <= 0) {
+        this.deleteitemcart(id)
+      }
+      this.updatetotalprice()
+    },
+    deleteitemcart(id) {
+      console.log("click to delete button element", id)
+
+      const item = this.itemList.find(item => item.id === id)
+      item.number = 0
+
+      // delete item into cartItemList with id = id
+      this.cartItemList = this.cartItemList.filter(item => item.id!== id)
+      // // update total
+      this.updatetotalprice()
+    },
+    updatetotalprice(){
+      let total = 0
+      this.cartItemList.forEach(item => {
+        total+= item.price * item.number
+      })
+      this.total = total
     }
   },
   mounted(){
